ac90c299a1114dfb9e9e307eda1d5ffbd1aacf1a
ereyes <ereyes@moneystop.io>
2025-02-20 13:21:38 -0500

    successfully got python server running

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
+    # Add a root URL pattern that redirects to onlinecourse
+    path('', lambda request: redirect('onlinecourse/')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
